ac34ab8732172b5f75db76b49b58469b9379698d
Omegadrien <omegadrien@gmail.com>
2017-11-13 11:18:19 +0100

    add category entries (see seed.db), article creation should work

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#### Catgory table ####
+Category.create(name: 'Food')
+Category.create(name: 'Work')
+Category.create(name: 'Games')
+Category.create(name: 'Tavel')
+Category.create(name: 'miscellaneous')
