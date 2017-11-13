36a7a98a89701de8ada0440c527ae070f2e3d1b6
Omegadrien <omegadrien@gmail.com>
2017-11-13 13:13:53 +0100

    update unit test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
-class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+describe User do
+
+  it "isAdmin should always return true" do
+    assert_equal User.isAdmin?, true
+  end
+
+  it "will not create the Article if the title is empty" do
+    article = Article.new()
+    assert_not article.save
+  end
+
 end
