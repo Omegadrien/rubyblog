49633385dc2b5023b36ac1d90ee7a402b9c27368
Omegadrien <omegadrien@gmail.com>
2017-11-13 11:01:49 +0100

    add new html to add a new article, add category model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'homepage/index'
+
+  resources :articles
+
+  root 'homepage#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
