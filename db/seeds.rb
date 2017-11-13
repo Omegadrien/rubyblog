4c57e2866486c5a36f3dd65d4eded31a9feb58a6
Omegadrien <omegadrien@gmail.com>
2017-11-13 12:45:18 +0100

    add message error in case the user don't validate the creation of an Article

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,12 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 #### Catgory table ####
-# Category.create(name: 'Food')
-# Category.create(name: 'Work')
-# Category.create(name: 'Games')
-# Category.create(name: 'Tavel')
-# Category.create(name: 'Miscellaneous')
+# Category.create(name: 'Food', id: 1)
+# Category.create(name: 'Work', id: 2)
+# Category.create(name: 'Games', id: 3)
+# Category.create(name: 'Tavel', id: 4)
+# Category.create(name: 'Miscellaneous', id: 5)
+
+#### Article table ####
+# Article.create(title: 'First Article title', text: 'First article content', category_id: 1)
+# Article.create(title: 'Second Article title', text: 'Second article content', category_id: 2)
