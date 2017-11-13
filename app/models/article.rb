6f428fd25452e389e9834b199591f9e9d1217b20
Omegadrien <omegadrien@gmail.com>
2017-11-13 12:14:53 +0100

    add validations for article, add link to add a new article in index view

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,7 @@
 class Article < ApplicationRecord
   belongs_to :category
+
+  validates :title, presence: true, length: { minimum: 3 }, uniqueness: true
+  validates :text, presence: true, length: {minimum: 15}
+  validates :category_id, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   resources :articles
 
-  root 'homepage#index'
+  root 'articles#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
